f20ba568ca25cc79ddb0520a8d4f6543a60774eb
Julio Muller <juliodolago@gmail.com>
2020-12-16 14:45:09 -0300

    Configured linting tool

--- src/dist.js
+++
@@ -1,1 +0,0 @@
-window.numOnly = require('./index')
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-/**
- * Removes any non-numeric character from a string chaing
- *
- * @param {string} target
- * @returns {string} - Target string you want to clear from NaN characters
- */
-module.exports = function(target) {
 
-  return String(target).replace(/\D/g, '')
-}
+const numOnly = (target) => {
+  return String(target).replace(/\D/g, '');
+};
+
+export default numOnly;
