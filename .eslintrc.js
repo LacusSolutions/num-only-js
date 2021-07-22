b4377059f7c03a00eee0faebc034f4822d0094c7
Julio Muller <juliodolago@gmail.com>
2021-07-21 22:30:38 -0300

    Update dependencies and switched to rollup bundler

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+/* eslint-env node */
+module.exports = {
+  env: {
+    'shared-node-browser': true,
+    'es6': true,
+    'jest': true,
+  },
+  extends: [
+    'lacussoft',
+    'lacussoft/typescript',
+  ],
+  ignorePatterns: [
+    'build/',
+    'dist/',
+    '!.babelrc.js',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+}
