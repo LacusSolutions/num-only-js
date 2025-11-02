049a5325e5289e3ba296088b2411c330ccf4b635
juliolmuller <juliolmuller@outlook.com>
2025-11-02 08:13:01 -0300

    test: separate Act from Assert codes

--- test/num-only.test.ts
+++ test/num-only.test.ts
@@ -3,49 +3,73 @@ import { expect, test } from 'bun:test';
 import numOnly from '..';
 
 test('Arg "Testing with string" returns empty', () => {
-  expect(numOnly('Testing with string')).toBe('');
+  const result = numOnly('Testing with string');
+
+  expect(result).toBe('');
 });
 
 test('Arg "testing123" returns "123"', () => {
-  expect(numOnly('testing123')).toBe('123');
+  const result = numOnly('testing123');
+
+  expect(result).toBe('123');
 });
 
 test('Arg "123.456.789-10" returns "12345678910"', () => {
-  expect(numOnly('123.456.789-10')).toBe('12345678910');
+  const result = numOnly('123.456.789-10');
+
+  expect(result).toBe('12345678910');
 });
 
 test('Arg ["a", 1, "2"] returns "12"', () => {
-  expect(numOnly(['a', 1, '2'])).toBe('12');
+  const result = numOnly(['a', 1, '2']);
+
+  expect(result).toBe('12');
 });
 
 test('Arg Infinity returns empty', () => {
-  expect(numOnly(Infinity)).toBe('');
+  const result = numOnly(Infinity);
+
+  expect(result).toBe('');
 });
 
 test('Arg undefined returns empty', () => {
-  expect(numOnly(undefined)).toBe('');
+  const result = numOnly(undefined);
+
+  expect(result).toBe('');
 });
 
 test('Arg null returns empty', () => {
-  expect(numOnly(null)).toBe('');
+  const result = numOnly(null);
+
+  expect(result).toBe('');
 });
 
 test('Arg { a: 1, b: 2 } returns empty', () => {
-  expect(numOnly({ a: 1, b: 2 })).toBe('');
+  const result = numOnly({ a: 1, b: 2 });
+
+  expect(result).toBe('');
 });
 
 test('Arg { toString: () => 123 } returns "123"', () => {
-  expect(numOnly({ toString: () => 123 })).toBe('123');
+  const result = numOnly({ toString: () => 123 });
+
+  expect(result).toBe('123');
 });
 
 test('Arg Date returns empty', () => {
-  expect(numOnly(Date)).toBe('');
+  const result = numOnly(Date);
+
+  expect(result).toBe('');
 });
 
 test('Arg Error returns empty', () => {
-  expect(numOnly(Error)).toBe('');
+  const result = numOnly(Error);
+
+  expect(result).toBe('');
 });
 
 test('Arg () => "abc123" returns "123"', () => {
-  expect(numOnly(() => 'abc123')).toBe('123');
+  const result = numOnly(() => 'abc123');
+
+  expect(result).toBe('123');
 });
